f410063b5a655872c8b7b0a807c7bc2651d58039
johnsnowc <1270772142@qq.com>
2022-03-21 23:07:47 +0800

    menu

--- menu.go
+++ menu.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var str string
+	fmt.Println("Menu Program")
+	for {
+		fmt.Scan(&str)
+		switch str {
+		case "hello":
+			fmt.Println("hello")
+		case "world":
+			fmt.Println("world")
+		case "quit":
+			fmt.Print("ok,goodbye")
+			return
+		default:
+			fmt.Println("wrong choice!")
+		}
+	}
 }
